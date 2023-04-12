870ead67236e6f406cca212a3ed5f81598ab23ce
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-12 22:08:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
+{
+author: "Bill Gates",
+title: "The Road Ahead",
+readingStatus: true,
+},
+{
+author: "Steve Jobs",
+title: "Walter Isaacson",
+readingStatus: true,
+},
+{
+author: "Suzanne Collins",
+title: "Mockingjay: The Final Book of The Hunger Games",
+readingStatus: false,
+},
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+let ans = 0;
+for (var i = 0; i < library.length; i++) {
+if (library[i].readingStatus) {
+ans++;
+}
+}
+return ans;
 };
 
 // Do not change the code below
